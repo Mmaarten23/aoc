6c64d751de0513b1216501cf3db589904b52b8c8
Maarten Magits <mmaartendisc@gmail.com>
2022-12-02 00:28:10 +0100

    [*] Make it look pwetty

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,14 +1,12 @@
 def main():
     text = open('input1.txt', 'r').read()
-    maximum = []
-    running_total = 0
-    for line in text.split('\n'):
-        if line == '':
-            maximum.append(running_total)
-            running_total = 0
-        else:
-            running_total += int(line)
-    print(sum(sorted(maximum, reverse=True)[:3]))
+    sums = sorted(get_sums(text), reverse=True)
+    print(f'Part 1: {sums[0]}')
+    print(f'Part 2: {sum(sums[:3])}')
+
+
+def get_sums(text:str):
+    return [sum([int(calorie) for calorie in calories.split('\n')]) for calories in text.split("\n\n")]
 
 
 if __name__ == '__main__':
