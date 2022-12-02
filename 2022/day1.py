0c283bf7b2190c25b961b990366e05fa2b3bdb15
Maarten Magits <mmaartendisc@gmail.com>
2022-12-02 13:13:00 +0100

    [+] 2022 2

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,11 +1,11 @@
 def main():
-    text = open('input1.txt', 'r').read()
-    sums = sorted(get_sums(text), reverse=True)
+    text: str = open('input1.txt', 'r').read()
+    sums: list[int] = sorted(get_sums(text), reverse=True)
     print(f'Part 1: {sums[0]}')
     print(f'Part 2: {sum(sums[:3])}')
 
 
-def get_sums(text:str):
+def get_sums(text: str) -> list[int]:
     return [sum([int(calorie) for calorie in calories.split('\n')]) for calories in text.split("\n\n")]
 
 
---
+++ 2022/day2.py
@@ -0,0 +1,42 @@
+opponent_choices: list[str] = ["A", "B", "C"]
+me_choices: list[str] = ["X", "Y", "Z"]
+
+
+def main():
+    text: list[str] = open('input2.txt', 'r').read().split("\n")
+    score_1: int = 0
+    score_2: int = 0
+    for line in text:
+        (opponent, me) = line.split(" ")
+        score_1 += get_score(me)
+        score_1 += get_result(opponent, me)
+
+        my_play = get_play(opponent, me)
+        score_2 += get_score(my_play)
+        score_2 += get_result(opponent, my_play)
+    print(f'Part 1: {score_1}')
+    print(f'Part 2: {score_2}')
+
+
+def get_score(me: str) -> int:
+    return {"X": 1, "Y": 2, "Z": 3}[me]
+
+
+def get_result(opponent: str, me: str) -> int:
+    if opponent_choices.index(opponent) == (me_choices.index(me) - 1) % 3:
+        return 6
+    if opponent_choices.index(opponent) == (me_choices.index(me) + 1) % 3:
+        return 0
+    return 3
+
+
+def get_play(opponent: str, me: str) -> str:
+    if me == "Z":
+        return me_choices[(opponent_choices.index(opponent) + 1) % 3]
+    if me == "X":
+        return me_choices[(opponent_choices.index(opponent) - 1) % 3]
+    return me_choices[opponent_choices.index(opponent)]
+
+
+if __name__ == '__main__':
+    main()
