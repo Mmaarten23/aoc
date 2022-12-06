c3eae0d5d29ee12038829fce837928a9d4a1acc7
Maarten Magits <mmaartendisc@gmail.com>
2022-12-06 14:28:07 +0100

    [+] 2015 2

---
+++ 2015/day2.py
@@ -0,0 +1,17 @@
+def main():
+    lines: list[str] = open('input2.txt').readlines()
+    part_1: int = 0
+    part_2: int = 0
+    for line in lines:
+        l, w, h = [int(x) for x in line.strip().split('x')]
+        part_1 += 2 * l * w + 2 * w * h + 2 * h * l + min(l * w, w * h, h * l)
+        small_1 = min(l, w, h)
+        small_2 = sorted([l, w, h])[1]
+        part_2 += small_1 + small_1 + small_2 + small_2 + l * w * h
+
+    print(f'Part 1: {part_1}')
+    print(f'Part 2: {part_2}')
+
+
+if __name__ == '__main__':
+    main()
